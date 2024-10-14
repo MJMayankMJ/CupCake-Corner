523a8d3e94b8cd6ed48c36c440a63b7596cb9a19
MJMayankMJ <226301131@gkv.ac.in>
2024-10-14 23:45:31 +0530

    Initial Commit

---
+++ CupCake CornerTests/CupCake_CornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupCake_CornerTests.swift
+//  CupCake CornerTests
+//
+//  Created by Mayank Jangid on 10/14/24.
+//
+
+import Testing
+@testable import CupCake_Corner
+
+struct CupCake_CornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
